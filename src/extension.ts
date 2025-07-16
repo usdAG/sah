ead466cb18135cb256427aeae1904cc0bac25f5f
MEntleutner <max.entleutner@usd.de>
2025-07-16 10:32:19 +0200

    Updated dependencies  - vsce moved to @vscode/vsce  - no vulns

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,4 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import * as vscode from 'vscode';
 import * as path from 'path';
 import generateMatchesWebview, {
